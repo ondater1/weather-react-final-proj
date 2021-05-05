78cec0fd1e49e353ee92a1e098d4a1deb6733bff
ondater1 <77303472+ondater1@users.noreply.github.com>
2021-05-05 21:48:38 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="Container">
+      <h1>Weather App</h1>
       <footer>
       <a href = "https://github.com/ondater1/weather-react-final-proj" target = "_blank" rel="noreferrer">Open source</a> coded by <a href = "https://www.linkedin.com/in/alla-strakhova-aa01a98a/" target = "_blank" rel="noreferrer">Alla Strakhova</a>
       </footer>
+    </div>
     </div>
   );
 }
