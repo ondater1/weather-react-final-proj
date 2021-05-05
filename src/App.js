522969743847303cf221dacf41348465324093d7
ondater1 <77303472+ondater1@users.noreply.github.com>
2021-05-05 22:19:28 +0200

    HTML structure added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <div className="Container">
       <h1>Weather App</h1>
+      <Weather/>
       <footer>
       <a href = "https://github.com/ondater1/weather-react-final-proj" target = "_blank" rel="noreferrer">Open source</a> coded by <a href = "https://www.linkedin.com/in/alla-strakhova-aa01a98a/" target = "_blank" rel="noreferrer">Alla Strakhova</a>
       </footer>
