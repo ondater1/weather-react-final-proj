f082914e8fc30a897182e1fd862952d38e343797
ondater1 <77303472+ondater1@users.noreply.github.com>
2021-05-05 19:57:47 +0200

    Footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+      <a href = "https://github.com/ondater1/weather-react-final-proj" target = "_blank" rel="noreferrer">Open source</a> coded by <a href = "https://www.linkedin.com/in/alla-strakhova-aa01a98a/" target = "_blank" rel="noreferrer">Alla Strakhova</a>
+      </footer>
     </div>
   );
 }
