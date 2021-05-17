c8caac5cc9e0231b00576418c368b47657641c59
ondater1 <77303472+ondater1@users.noreply.github.com>
2021-05-17 11:43:35 +0300

    amended the footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ export default function App() {
       <h1>Calm Weather</h1>
       <Weather defaultCity="Prague"/>
       <footer>
-      <a href = "https://github.com/ondater1/weather-react-final-proj" target = "_blank" rel="noreferrer">Open source</a> coded by <a href = "https://www.linkedin.com/in/alla-strakhova-aa01a98a/" target = "_blank" rel="noreferrer">Alla Strakhova</a>
+      The project was coded by <a href = "https://www.linkedin.com/in/alla-strakhova-aa01a98a/" target = "_blank" rel="noreferrer">Alla Strakhova</a>
+      , it is <a href = "https://github.com/ondater1/weather-react-final-proj" target = "_blank" rel="noreferrer">open-sourced on Github </a>
+       and <a href = "https://determined-meninsky-ef0708.netlify.app/" target = "_blank" rel="noreferrer">hosted on Netlify</a>. Enjoy!
       </footer>
     </div>
     </div>
